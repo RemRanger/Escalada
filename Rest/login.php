98aedc82c739a42c5f4bd754b329b89b2053dbe7
RemRanger <rxb@ziggo.nl>
2019-04-09 17:18:47 +0200

    Login (1).

---
+++ Rest/login.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
+session_start();
+include_once 'dbconnect.php';
+
+// if (isset($_SESSION['IdUser']) != "")
+// {
+	// header("Location: index.php");
+	// //echo 'Logged in already';
+// }
+if(isset($_POST['btn-login']))
+{
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$upass = mysqli_real_escape_string($conn, $_POST['pass']);
+
+	$res=$conn->query("SELECT Id, Password FROM User WHERE UserName='$username'");
+	$row=$res->fetch_assoc();
+	if($row['Password'] == md5($upass))
+		echo '{"id": ' . $row['Id'] . '}';
+	else
+		echo '{"id": -1}';
+}
+else
+	echo '{"id": -1';
+?>
